8ebd2e906eaa0186359954649cd8f1912dc5e390
Oli G <oli@Seneca.local>
2025-06-04 07:33:18 +0200

    minor update

--- scripts/grepluakeymaps.sh
+++ scripts/grepluakeymaps.sh
@@ -5,14 +5,11 @@
 # Search recursively for lines containing keymap definitions with <C- or <leader>
 rg --no-filename --no-line-number 'keymap\..*(<C-|<leader>)' ~/.config/nvim |
 
-# Exclude lines that only contain comments with <C- or <leader>
+# Exclude lines that contain comments followed by <C- or <leader>
 grep -v '\-\-.*\(<C-\|<leader>\)' |
 
 # Remove leading whitespace or tabs
 sed 's/^[ \t]*//' |
-
-# Remove 'vim.keymap.set(' prefix
-sed 's/vim.keymap.set(//' |
 
 # Sort by second field (the key combination), then by first (mode)
 sort -t',' -k2,2 -k1,1 |
