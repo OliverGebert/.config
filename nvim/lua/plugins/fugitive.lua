ac444d672c0736578853bf84faf61485a1b1b76c
Oli G <oli@Seneca.fritz.box>
2025-08-18 18:47:29 +0200

    update

--- nvim/lua/plugins/fugitive.lua
+++ nvim/lua/plugins/fugitive.lua
@@ -8,7 +8,7 @@ return {
   },
   config = function()
     vim.keymap.set('n', '<leader>gs', "<cmd>G<CR>", { desc = "Git Status (Fugitive)" })
-    vim.keymap.set('n', '<leader>gd', "<cmd>Gdiff<CR>", { desc = "Git Diff (HEAD vs current)" })
+    vim.keymap.set('n', '<leader>gd', "<cmd>Gvdiffsplit<CR>", { desc = "Git Diff (HEAD vs current)" })
     vim.keymap.set('n', '<leader>gb', "<cmd>Git blame<CR>", { desc = "Git Blame" })
     vim.keymap.set('n', '<leader>gl', "<cmd>Git log<CR>", { desc = "Git Log" })
     vim.keymap.set('n', '<leader>gc', "<cmd>Git commit -v<CR>", { desc = "Git Commit" })
